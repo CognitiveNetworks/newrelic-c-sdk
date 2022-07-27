47b29d12e603e10468fa00590b03435e932650a3
andrew.moore <andrew.moore@vizio.com>
2022-07-27 14:06:38 +0000

    Simplified the New Relic Makefile module

--- make/newrelic.mk
+++ make/newrelic.mk
@@ -1,14 +1,10 @@
 # New Relic Makefile Module
 
 CFLAGS += -I./newrelic
+CFLAGS += -DNEWRELIC_VERSION=\"$(shell cat ./newrelic/VERSION)\"
 
 ifeq ($(USE_NEWRELIC), 1)
-NEWRELIC_VERSION := $(shell if test -f ./newrelic/VERSION; then cat ./newrelic/VERSION; fi)
-
 CFLAGS += -DNEWRELIC_ENABLE
-CFLAGS += -DNEWRELIC_VERSION=\"$(NEWRELIC_VERSION)\"
 LIBS   += -L./newrelic -l:libnewrelic.a -lpcre -rdynamic
-else
-CFLAGS += -DNEWRELIC_VERSION=\"new.relic.disabled\"
 endif
 
